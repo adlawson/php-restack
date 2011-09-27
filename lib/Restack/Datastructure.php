e5f5a5e57f96b2b5542f20e208f51c11643d6618
adlawson <adlawson@gmail.com>
2011-09-27 01:26:27 +0100

    Added datastructure interface

---
+++ lib/Restack/Datastructure.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Restack;
+
+/**
+ * @category  Restack
+ * @package   Restack\Datastructure
+ */
+interface Datastructure
+{
+    /**
+     * Clear storage
+     * @return void
+     */
+    public function clear();
+    
+    /**
+     * Check if an item exists in storage
+     * @param mixed $item
+     * @return boolean
+     */
+    public function exists($item);
+    
+    /**
+     * Insert an item into storage
+     * @param mixed $item
+     * @return void
+     */
+    public function insert($item);
+    
+    /**
+     * Remove an item from storage
+     * @param mixed $item
+     * @return void
+     */
+    public function remove($item);
+}
