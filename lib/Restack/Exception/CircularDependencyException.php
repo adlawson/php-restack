0c28956bc331b99610c9ba313e952b9de3843877
adlawson <adlawson@gmail.com>
2011-10-04 01:43:47 +0100

    Reverted deleted exception class

---
+++ lib/Restack/Exception/CircularDependencyException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Restack\Exception;
+
+/**
+ * @category  Restack
+ * @package   Restack\Exception
+ */
+class CircularDependencyException extends InvalidArgumentException implements \Restack\Exception
+{
+    
+}
